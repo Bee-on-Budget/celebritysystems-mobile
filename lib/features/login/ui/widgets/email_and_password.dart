c6e0ca1dca4bbb5f479e5ed02942cbea806df6f5
sami-MhdAmin <samimhdamin@gmail.com>
2025-07-22 05:23:14 +0300

    change login design

--- lib/features/login/ui/widgets/email_and_password.dart
+++ lib/features/login/ui/widgets/email_and_password.dart
@@ -66,3 +66,197 @@ class _EmailAndPasswordState extends State<EmailAndPassword> {
     super.dispose();
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+
+// import '../../../../core/theming/colors.dart';
+// import '../../logic/login cubit/login_cubit.dart';
+
+// class EmailAndPassword extends StatefulWidget {
+//   const EmailAndPassword({super.key});
+
+//   @override
+//   State<EmailAndPassword> createState() => _EmailAndPasswordState();
+// }
+
+// class _EmailAndPasswordState extends State<EmailAndPassword>
+//     with TickerProviderStateMixin {
+//   bool isObscure = true;
+//   late TextEditingController passwordController;
+//   late AnimationController _animationController;
+//   late Animation<double> _fadeAnimation;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     passwordController = context.read<LoginCubit>().passwordController;
+
+//     _animationController = AnimationController(
+//       duration: const Duration(milliseconds: 600),
+//       vsync: this,
+//     );
+
+//     _fadeAnimation = Tween<double>(
+//       begin: 0.0,
+//       end: 1.0,
+//     ).animate(CurvedAnimation(
+//       parent: _animationController,
+//       curve: Curves.easeInOut,
+//     ));
+
+//     _animationController.forward();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FadeTransition(
+//       opacity: _fadeAnimation,
+//       child: Form(
+//         key: context.read<LoginCubit>().formKey,
+//         child: Column(
+//           children: [
+//             // Enhanced Email Input
+//             _buildEnhancedTextField(
+//               label: "Email Address",
+//               icon: Icons.email_outlined,
+//               controller: context.read<LoginCubit>().emailController,
+//               isPassword: false,
+//             ),
+
+//             const SizedBox(height: 24),
+
+//             // Enhanced Password Input
+//             _buildEnhancedTextField(
+//               label: "Password",
+//               icon: Icons.lock_outline,
+//               controller: context.read<LoginCubit>().passwordController,
+//               isPassword: true,
+//               obscureText: isObscure,
+//               toggleObscure: () {
+//                 setState(() => isObscure = !isObscure);
+//               },
+//             ),
+
+//             const SizedBox(height: 16),
+
+//             // Enhanced Forgot Password
+//             Align(
+//               alignment: Alignment.centerRight,
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(8),
+//                 ),
+//                 child: TextButton(
+//                   onPressed: () {},
+//                   style: TextButton.styleFrom(
+//                     padding:
+//                         const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(8),
+//                     ),
+//                   ),
+//                   child: Text(
+//                     "Forgot Password?",
+//                     style: TextStyle(
+//                       color: ColorsManager.royalIndigo,
+//                       fontWeight: FontWeight.w500,
+//                       fontSize: 14,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildEnhancedTextField({
+//     required String label,
+//     required IconData icon,
+//     required TextEditingController controller,
+//     required bool isPassword,
+//     bool obscureText = false,
+//     VoidCallback? toggleObscure,
+//   }) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         color: Colors.white,
+//         borderRadius: BorderRadius.circular(16),
+//         border: Border.all(
+//           color: Colors.grey.shade200,
+//           width: 1.5,
+//         ),
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.black.withOpacity(0.04),
+//             blurRadius: 8,
+//             offset: const Offset(0, 2),
+//           ),
+//         ],
+//       ),
+//       child: TextFormField(
+//         controller: controller,
+//         obscureText: isPassword ? obscureText : false,
+//         decoration: InputDecoration(
+//           labelText: label,
+//           labelStyle: TextStyle(
+//             color: Colors.grey.shade600,
+//             fontWeight: FontWeight.w500,
+//           ),
+//           prefixIcon: Container(
+//             margin: const EdgeInsets.all(12),
+//             padding: const EdgeInsets.all(8),
+//             decoration: BoxDecoration(
+//               color: ColorsManager.royalIndigo.withOpacity(0.1),
+//               borderRadius: BorderRadius.circular(8),
+//             ),
+//             child: Icon(
+//               icon,
+//               color: ColorsManager.royalIndigo,
+//               size: 20,
+//             ),
+//           ),
+//           suffixIcon: isPassword
+//               ? IconButton(
+//                   onPressed: toggleObscure,
+//                   icon: AnimatedSwitcher(
+//                     duration: const Duration(milliseconds: 200),
+//                     child: Icon(
+//                       obscureText ? Icons.visibility_off : Icons.visibility,
+//                       key: ValueKey(obscureText),
+//                       color: Colors.grey.shade600,
+//                     ),
+//                   ),
+//                 )
+//               : null,
+//           border: InputBorder.none,
+//           enabledBorder: InputBorder.none,
+//           focusedBorder: OutlineInputBorder(
+//             borderRadius: BorderRadius.circular(16),
+//             borderSide: BorderSide(
+//               color: ColorsManager.royalIndigo,
+//               width: 2,
+//             ),
+//           ),
+//           contentPadding:
+//               const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+//         ),
+//         style: TextStyle(
+//           fontSize: 16,
+//           fontWeight: FontWeight.w500,
+//           color: ColorsManager.graphiteBlack,
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _animationController.dispose();
+//     passwordController.dispose();
+//     super.dispose();
+//   }
+// }
--- lib/features/login/ui/widgets/login_header.dart
+++ lib/features/login/ui/widgets/login_header.dart
@@ -1,33 +1,163 @@
+// import 'package:celebritysystems_mobile/core/theming/colors.dart';
+// import 'package:flutter/material.dart';
+
+// class LoginHeader extends StatelessWidget {
+//   const LoginHeader({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Column(
+//         children: [
+//           const SizedBox(height: 30),
+//           Image.asset(
+//             'assets/images/logo.png',
+//             height: 80,
+//           ),
+//           const SizedBox(height: 16),
+//           Text(
+//             "Welcome Back",
+//             style: TextStyle(
+//               fontSize: 24,
+//               fontWeight: FontWeight.bold,
+//               color: ColorsManager.graphiteBlack,
+//             ),
+//           ),
+//           const SizedBox(height: 8),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:celebritysystems_mobile/core/theming/colors.dart';
 import 'package:flutter/material.dart';
 
-class LoginHeader extends StatelessWidget {
-  const LoginHeader({
-    super.key,
-  });
+class LoginHeader extends StatefulWidget {
+  const LoginHeader({super.key});
+
+  @override
+  State<LoginHeader> createState() => _LoginHeaderState();
+}
+
+class _LoginHeaderState extends State<LoginHeader>
+    with TickerProviderStateMixin {
+  late AnimationController _pulseController;
+  late AnimationController _slideController;
+  late Animation<double> _pulseAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _pulseController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _slideController = AnimationController(
+      duration: const Duration(milliseconds: 800),
+      vsync: this,
+    );
+
+    _pulseAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.1,
+    ).animate(CurvedAnimation(
+      parent: _pulseController,
+      curve: Curves.easeInOut,
+    ));
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _slideController,
+      curve: Curves.elasticOut,
+    ));
+
+    _slideController.forward();
+    _pulseController.repeat(reverse: true);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const SizedBox(height: 30),
-          Image.asset(
-            'assets/images/logo.png',
-            height: 80,
-          ),
-          const SizedBox(height: 16),
-          Text(
-            "Welcome Back",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: ColorsManager.graphiteBlack,
+    return SlideTransition(
+      position: _slideAnimation,
+      child: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+
+            // Animated Logo with pulse effect
+            ScaleTransition(
+              scale: _pulseAnimation,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: ColorsManager.royalIndigo.withOpacity(0.2),
+                      blurRadius: 20,
+                      offset: const Offset(0, 8),
+                    ),
+                  ],
+                ),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  height: 60,
+                ),
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-        ],
+
+            const SizedBox(height: 24),
+
+            // Enhanced welcome text with gradient
+            ShaderMask(
+              shaderCallback: (bounds) => LinearGradient(
+                colors: [
+                  ColorsManager.graphiteBlack,
+                  ColorsManager.coralBlaze,
+                ],
+              ).createShader(bounds),
+              child: Text(
+                "Welcome Back",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: -0.5,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 8),
+
+            Text(
+              "Sign in to continue",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey.shade600,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _pulseController.dispose();
+    _slideController.dispose();
+    super.dispose();
   }
 }
