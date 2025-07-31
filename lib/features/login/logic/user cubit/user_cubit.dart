6955304e76651834399ec850e167cbb5b526b34c
Salwan Arar <salwan.arar@gmail.com>
2025-07-31 16:03:19 +0300

    feat: implement logout functionality
    This commit adds a logout method to the UserCubit.
    
    The logout method clears all data from shared preferences and emits null to reset the user state. This effectively logs the user out of the application.

--- lib/features/login/logic/user cubit/user_cubit.dart
+++ lib/features/login/logic/user cubit/user_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:celebritysystems_mobile/core/helpers/shared_pref_helper.dart';
 import 'package:celebritysystems_mobile/core/helpers/user_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:convert';
@@ -11,11 +12,17 @@ class UserCubit extends Cubit<UserModel?> {
       final user = UserModel(
         username: payload['username'],
         role: payload['role'],
+        companyId: payload['companyId'],
       );
       emit(user);
     } catch (e) {
       emit(null);
     }
+  }
+
+  void logout() async {
+    await SharedPrefHelper.clearAllData();
+    emit(null);
   }
 
   Map<String, dynamic> _decodeJWT(String token) {
