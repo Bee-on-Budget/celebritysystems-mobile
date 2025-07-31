c97243ace23e5012792c8126d5c970f0fd57fd85
Salwan Arar <salwan.arar@gmail.com>
2025-07-31 16:04:08 +0300

    Add createCompanyTicketScreen route

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -3,6 +3,7 @@ class Routes {
   static const String loginScreen = '/loginScreen';
   static const String homeScreen = '/homeScreen';
   static const String companyHomeScreen = '/companyHomeScreen';
+  static const String createCompanyTicketScreen = '/createCompanyTicketScreen';
   static const String splashScreen = '/splashScreen';
   static const String reportScreen = '/reportScreen';
 }
