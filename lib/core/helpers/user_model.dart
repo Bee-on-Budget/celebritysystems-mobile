65d5744f0a4d3d6a864a6752b9674a25138fcda6
Salwan Arar <salwan.arar@gmail.com>
2025-07-31 16:03:05 +0300

    feat: Add companyId to UserModel and update isAllowed logic

--- lib/core/helpers/user_model.dart
+++ lib/core/helpers/user_model.dart
@@ -1,8 +1,13 @@
 class UserModel {
   final String username;
   final String role;
+  final int? companyId;
 
-  UserModel({required this.username, required this.role});
+  UserModel({
+    required this.username,
+    required this.role,
+    required this.companyId,
+  });
 
   bool get isAllowed => role == 'CELEBRITY_SYSTEM_WORKER' || role == 'COMPANY';
 }
