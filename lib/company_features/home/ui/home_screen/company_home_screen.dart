5d0a3697a84305b20bc1b38b03515ed4f2301960
Salwan Arar <salwan.arar@gmail.com>
2025-07-31 16:07:49 +0300

    Create `CompanyHomeScreen` with AppBar, Body and FloatingActionButton
    The `CompanyHomeScreen` initializes by fetching company data based on the logged-in user's company ID. It includes a refresh functionality to reload this data.
    
    The UI consists of:
    - A custom AppBar (`companyHomeAppBar`).
    - The main content area (`companyHomeBody`).
    - A FloatingActionButton that navigates to the `CreateCompanyTicketScreen`.

---
+++ lib/company_features/home/ui/home_screen/company_home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/routing/routes.dart';
+import '../../../../core/theming/colors.dart';
+import '../../../../features/login/logic/user cubit/user_cubit.dart';
+import '../../logic/company_home_cubit/company_home_cubit.dart';
+import 'components/company_home_app_bar.dart';
+import 'components/company_home_body.dart';
+
+class CompanyHomeScreen extends StatefulWidget {
+  const CompanyHomeScreen({super.key});
+
+  @override
+  State<CompanyHomeScreen> createState() => _CompanyHomeScreenState();
+}
+
+class _CompanyHomeScreenState extends State<CompanyHomeScreen> {
+  late final int _companyId;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _companyId = context.read<UserCubit>().state?.companyId ?? 0;
+    context.read<CompanyHomeCubit>().loadCompanyHomeData(_companyId);
+  }
+
+  Future<void> _onRefresh() async {
+    context.read<CompanyHomeCubit>().loadCompanyHomeData(_companyId);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorsManager.mistWhite,
+      appBar: companyHomeAppBar(
+        context,
+        ColorsManager.coralBlaze,
+      ),
+      body: companyHomeBody(_onRefresh),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, Routes.createCompanyTicketScreen);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
