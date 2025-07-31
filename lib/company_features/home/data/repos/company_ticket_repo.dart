41163ac45f8a12f051c2b81cd0fac20e3e3c0b60
Salwan Arar <salwan.arar@gmail.com>
2025-07-31 16:06:49 +0300

    feat: implement company ticket repository
    This commit introduces the `CompanyTicketRepo` class, which handles API interactions for company tickets.
    
    It includes methods to:
    - Get all company tickets.
    - Get a specific ticket by its ID.
    - Update a ticket.
    
    Error handling is implemented using `ApiResult` and `ErrorHandler`.

---
+++ lib/company_features/home/data/repos/company_ticket_repo.dart
@@ -0,0 +1,40 @@
+import '../../../../core/networking/api_error_handler.dart';
+import '../../../../core/networking/api_result.dart';
+import '../api/company_tickets_api_service.dart';
+import '../models/company_tickets_response.dart';
+
+class CompanyTicketRepo {
+  final CompanyTicketsApiService _companyTicketApiService;
+
+  CompanyTicketRepo(this._companyTicketApiService);
+
+  Future<ApiResult<List<CompanyTicketResponse>>> getCompanyTickets(
+      int companyId) async {
+    try {
+      final List<CompanyTicketResponse> response =
+      await _companyTicketApiService.getCompanyTickets(companyId);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<CompanyTicketResponse>> getTicketById(int ticketId) async {
+    try {
+      final CompanyTicketResponse response =
+      await _companyTicketApiService.getTicket(ticketId);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+
+  Future<ApiResult<void>> putTicket(CompanyTicketResponse ticket) async {
+    try {
+      await _companyTicketApiService.putTicket(ticket);
+      return const ApiResult.success(null);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
