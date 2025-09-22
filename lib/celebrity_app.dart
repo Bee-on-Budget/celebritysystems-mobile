dfe5eed9703a0150cfec89a436d43cdc25a91568
sami-MhdAmin <samimhdamin@gmail.com>
2025-09-23 01:17:29 +0300

    final push berfore merging with main, this branch includes subcontract implementation plus some bug fixes

--- lib/celebrity_app.dart
+++ lib/celebrity_app.dart
@@ -1,5 +1,4 @@
 import 'package:celebritysystems_mobile/core/language_cubit/language_cubit.dart';
-import 'package:celebritysystems_mobile/settings_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
